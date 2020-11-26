c87005269fd1498eb0340309ce7441fd38196c3e
ALTAEUN_ALHILBIU <75084761+AltaeunAlhilbiu@users.noreply.github.com>
2020-11-26 17:22:18 +0200

    Add files via upload

---
+++ ALTAEUN_ALHILBIU_WhatsApp.py
@@ -0,0 +1,13 @@
+import os
+os.system("clear")
+print("\033[0;34m Loading........... .")
+print (" \033[0;31m HACK WHATSAPP BY ALTAEUN_ALHILBIU")
+from random import choices
+ALTAEUN_ALHILBIU = input ("inter your phone numper to hack : ")
+list=["(123467","765490","979878","010963","264790","101009","389118","144340","285927","112628","730695","432262","708626","981828","514744","896633","837014","436655","303061","287738","191836)"]
+x=choices(list,k=5)
+print(x)
+print("\033[1;33m *_*_*_*_*_*_*_*_*_*_*")
+print("\033[1;32m (Copy the above codes and put the verification code on WhatsApp)")
+print("\033[1;33m *_*_*_*_*_*_*_*_*_*_*")
+print("\033[1;32m ((This tool estimates 50% Just))")
